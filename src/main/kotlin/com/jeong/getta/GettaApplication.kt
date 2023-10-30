1fa8a9d0434e3ff15c0425818ea4b3ada563f992
seohyun jeong <jeongseohyun31@gmail.com>
2023-10-30 10:21:09 +0900

    git init

---
+++ src/main/kotlin/com/jeong/getta/GettaApplication.kt
@@ -0,0 +1,11 @@
+package com.jeong.getta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GettaApplication
+
+fun main(args: Array<String>) {
+	runApplication<GettaApplication>(*args)
+}
---
+++ src/test/kotlin/com/jeong/getta/GettaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jeong.getta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GettaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
